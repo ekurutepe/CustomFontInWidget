b6b707da580869ebe44c48b2c5bd202fe44cb3a2
Engin Kurutepe <engin@kurutepe.com>
2020-07-10 16:33:19 +0200

    Initial Commit

---
+++ CustomFontInWidget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomFontInWidget
+//
+//  Created by Engin Kurutepe on 10.07.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
